46d164ef7a0f7bda1f262f51614cc1956ad706a2
loukai <943469483@qq.com>
2024-03-31 01:18:59 +0800

    feat: type类型不一致处理，initialize

---
+++ v05.2/App.jsx
@@ -0,0 +1,30 @@
+import React from "./core/React.js"
+
+let showBar = false
+function Counter() {
+    //注意，foo和bar的type不一样，一个是div,一个是p；
+    //如果type一样就没有问题了；type不一样，导致每次点击，页面都是新创建了一个出来
+    const foo = <div id='foo-id'>foo</div>
+    const bar = <p id='bar-id'>bar</p>
+
+    function handleShowBar() {
+        showBar = !showBar
+        React.update()
+    }
+    return (
+        <div>
+            Counter
+            <div>{showBar ? bar : foo}</div>
+            <button onClick={handleShowBar}>showBar</button>
+        </div>
+    )
+}
+function App() {
+    return (
+        <div>
+            hi-mini-react
+            <Counter></Counter>
+        </div>
+    )
+}
+export default App
