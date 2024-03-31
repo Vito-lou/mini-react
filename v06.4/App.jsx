038dc8545687689681a922498e6f8b3b17779f00
loukai <943469483@qq.com>
2024-03-31 18:43:12 +0800

    feat: 出现多个hook的时候，出问题了

--- v06.4/App.jsx
+++ v06.4/App.jsx
@@ -2,14 +2,18 @@ import React from "./core/React.js"
 
 function Foo() {
     const [count, setCount] = React.useState(10)
+    const [bar, setBar] = React.useState('bar')
     console.log(count, setCount)
     function handleClick() {
         setCount((c) => c + 1)
+        setBar(s => s + 'bar')
     }
+
     return (
         <div>
             <h1>foo</h1>
             {count}
+            <div>{bar}</div>
             <button onClick={handleClick}>click</button>
         </div>
     )
